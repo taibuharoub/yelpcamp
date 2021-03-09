1d8ec4f4bcc66cc0499d1b67ee4f60175be2f028
taibuharoub <haroubtaibu@gmail.com>
2021-03-09 08:56:21 +0300

    Delopy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-console.log(process.env.SECERT)
+// require("dotenv").config();
 
 const path = require("path");
 const express = require("express");
@@ -13,14 +13,19 @@ const methodOverride = require("method-override");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
+const MongoStore = require("connect-mongo").default;
 
 const ExpressError = require("./utils/ExpressError");
 const campgroundsRoutes = require("./routes/campground");
 const reviewRoutes = require("./routes/review");
 const User = require("./models/user");
 const userRoutes = require("./routes/users");
+const dbUrl ="mongodb://localhost:27017/yelp-camp"
+const secret = process.env.SECERT || "EEJE773225DDS"
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -34,14 +39,27 @@ db.once("open", () => {
 });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 *60
+})
+
+store.on("error",  function(e) {
+  console.log("Session Store Error", e)
+})
 
 const sessionConfig = {
-  secret: "lazydog",
+  store,
+  name: "cow",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true, //when we delop
     expires: Date.now() + 1000 * 60 * 24 *7,
     maxAge: 1000 * 60 * 24 *7
   }
@@ -53,9 +71,57 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")))
+app.use(mongoSanitize());
 app.use(methodOverride("_method"));
 app.use(session(sessionConfig));
 app.use(flash())
+
+app.use(helmet());
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://cdn.jsdelivr.net/",
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dqty2etfr/",
+              "https://images.unsplash.com/",
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -64,7 +130,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  console.log(req.session)
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success")
   res.locals.error = req.flash("error")
@@ -84,16 +149,14 @@ app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 
 app.all("*", (req, res, next) => {
-  // res.send("404!!!");
+  res.send("404!!!");
   next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   if (!err.message) err.message = "Something went wrong";
-  // res.status(statusCode).send(message)
   res.status(statusCode).render("error", { err });
-  // res.send("Something Went Wrong!")
 });
 
 app.listen(port, () => {
--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -14,8 +14,6 @@ map.on('load', function () {
 // add the point_count property to your source data.
 map.addSource('campgrounds', {
 type: 'geojson',
-// Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-// from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
 data: campgrounds,
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
