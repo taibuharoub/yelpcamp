4878bb5016471d414203856c5a7ce710c1f22f77
taibuharoub <haroubtaibu@gmail.com>
2021-03-09 09:40:56 +0300

    Evn vars

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const campgroundsRoutes = require("./routes/campground");
 const reviewRoutes = require("./routes/review");
 const User = require("./models/user");
 const userRoutes = require("./routes/users");
-const dbUrl ="mongodb://localhost:27017/yelp-camp"
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp"
 const secret = process.env.SECERT || "EEJE773225DDS"
 
 mongoose.connect(dbUrl, {
